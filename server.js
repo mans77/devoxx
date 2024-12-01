b9a0a08e450307e96ae9531c47034b67a3b32ef4
mansour gueye <sorogueye93@gmail.com>
2024-12-01 10:35:39 +0000

    hello

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const PORT = 3000;
+const HOST = "0.0.0.0";
+
+const app = express();
+app.get('/', (req,res)=>{
+    res.send("Bonjour les gars");
+});
+
+app.listen(PORT,HOST, ()=>{
+    console.log('execution sur le port' + PORT);
+});
+
